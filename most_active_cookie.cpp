6d70c9218bab9c06f4d60f8a3684f4d1fcbf47ef
HyunjoonJeon <hj1119@ic.ac.uk>
2022-05-19 03:45:23 +0100

    extractCookies now extracts cookies at given date. Added a function that compares two given dates

--- most_active_cookie.cpp
+++ most_active_cookie.cpp
@@ -5,6 +5,21 @@
 
 using namespace std;
 
+// returns false if date1 is earlier
+int8_t compareDates(string date1, string date2) {
+    int year1, month1, day1;
+    int year2, month2, day2;
+    sscanf(date1.data(), "%d-%d-%d", &year1, &month1, &day1);
+    sscanf(date2.data(), "%d-%d-%d", &year2, &month2, &day2);
+    if(year1 < year2 || year1 == year2 && month1 < month2 || year1 == year2 && month1 == month2 && day1 < day2) {
+        return -1;
+    } else if(date1 == date2) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
 // extract cookies in the given date
 bool extractCookies(string fileName, vector<string> &cookies, string date) {
     string line;
@@ -14,8 +29,12 @@ bool extractCookies(string fileName, vector<string> &cookies, string date) {
         while(getline(file, line)) {
             int splitPos = line.find(',');
             string cookie = line.substr(0, splitPos);
-            string cookieDate = line.substr(splitPos + 1);
-            if(cookieDate.substr(0,cookieDate.find('T')) == date) {
+            string cookieDateAndTime = line.substr(splitPos + 1);
+            string cookieDate = cookieDateAndTime.substr(0, cookieDateAndTime.find('T'));
+            int8_t compare = compareDates(cookieDate, date);
+            if(compare == -1) {
+                break;
+            } else if(compare == 0){
                 cookies.push_back(cookie);
             }
         }
