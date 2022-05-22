d0440dc8c982e51431f9f24c9d1945ec90da5535
HyunjoonJeon <hj1119@ic.ac.uk>
2022-05-22 02:49:02 +0100

    testcases are now written to a csv file and answers written to a txt file

--- generate_testcases.cpp
+++ generate_testcases.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <ctime>
 #include <vector>
+#include <fstream>
 
 using namespace std;
 
@@ -18,7 +19,7 @@ string randomString(unsigned int size) {
     return result;
 }
 
-void randomCookie(int nOfCookies, int nOfTestcases) {
+void randomCookie(int nOfCookies, int nOfTestcases, string fileName) {
     srand((int) time(0));
     string dates[5] = {
         "2022-05-21",
@@ -38,8 +39,14 @@ void randomCookie(int nOfCookies, int nOfTestcases) {
     for(int i = 0; i < nOfCookies * 5; i++) {
         frequencies.push_back(0);
     }
+    // create a cookies log and answer file
+    ofstream logFile;
+    logFile.open(fileName+".csv");
+    ofstream answerFile;
+    answerFile.open(fileName+"_answer.txt");
+
     // generate n number of testcases and count frequencies of the cookies for answer
-    cout << "cookie,timestamp" << endl;
+    logFile << "cookie,timestamp" << endl;
     for(int i = 0; i < nOfTestcases; i++) {
         int cookieIndex = rand() % nOfCookies;
         string cookie = cookies[cookieIndex];
@@ -47,18 +54,22 @@ void randomCookie(int nOfCookies, int nOfTestcases) {
         frequencies[cookieIndex + dateIndex * nOfCookies]++;
         cookie += "," + dates[dateIndex];
         cookie += time;
-        cout << cookie << endl;
+        logFile << cookie << endl;
     }
 
-    // print most active cookies for each date
+    // find most active cookies for each date
     for(int i = 0; i < 5; i++) {
-        cout << dates[i] << " " << cookies[(max_element(frequencies.begin()+i*nOfCookies, frequencies.begin()+(i+1)*nOfCookies) - frequencies.begin()) % nOfCookies] << endl;
+        answerFile << dates[i] << " " << cookies[(max_element(frequencies.begin()+i*nOfCookies, frequencies.begin()+(i+1)*nOfCookies) - frequencies.begin()) % nOfCookies] << endl;
     }
+
+    // close the files
+    logFile.close();
+    answerFile.close();
 }
 
 int main(int argc, char **argv) {
 
-    randomCookie(stoi(argv[1]), stoi(argv[2]));
+    randomCookie(stoi(argv[1]), stoi(argv[2]), argv[3]);
 
     return 0;
 }
