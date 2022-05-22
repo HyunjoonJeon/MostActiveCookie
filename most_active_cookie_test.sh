ca68aca911ed7eea29dcdaa989f5bae2d51f9012
HyunjoonJeon <hj1119@ic.ac.uk>
2022-05-22 05:54:58 +0100

    Bash script now sorts outputs before checking the answer

--- most_active_cookie_test.sh
+++ most_active_cookie_test.sh
@@ -23,7 +23,10 @@ DatesArray=("2022-05-21" "2022-05-20" "2022-05-19" "2022-05-18" "2022-05-17")
 for i in Program_tests/ManualTest/Testcases/*; do
     for dateIndex in "${!DatesArray[@]}"; do
         CHECKED=$(( ${CHECKED} + 1 ));
-        program=$(./most_active_cookie $i -d ${DatesArray[$dateIndex]})
+        ./most_active_cookie $i -d ${DatesArray[$dateIndex]} > tempFile
+        sort -o tempFile tempFile
+        program=$(<tempFile)
+        rm -f tempFile
         base=$(basename ${i} .csv)
         answer_file=Program_tests/ManualTest/Answers/${base}_answer_${DatesArray[$dateIndex]}.txt
         answer=$(<${answer_file})
